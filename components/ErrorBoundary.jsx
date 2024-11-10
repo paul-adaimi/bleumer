fb2d34941ee4c7286173ac086ea85dccdca29007
Paul Adaimi <adaimi.paul@gmail.com>
2024-11-10 11:50:23 -0500

    added Sentry and fixed deployment

--- components/ErrorBoundary.jsx
+++ components/ErrorBoundary.jsx
@@ -3,8 +3,6 @@ import { View, Text, Button, StyleSheet } from "react-native";
 
 // TODO: Make sure contact us number is right
 // TODO: look into notifications
-// TODO: look into crashlytics
-// TODO: fix this
 class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
